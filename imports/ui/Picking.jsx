c51b28a36f64a122f2498172b8a655c83374099b
bwozniak <bwozniak@umich.edu>
2021-08-23 23:17:53 -0400

    working

--- imports/ui/Picking.jsx
+++ imports/ui/Picking.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 export const Picking = () => {
@@ -8,7 +8,21 @@ export const Picking = () => {
 
   return (
     <div>
-      hello
+      <div className="my-2">
+        <h1 className="text-5xl tracking-wider font-bold text-center"> Pick what would win</h1>
+      </div>
+      <div className="flex justify-evenly my-4">
+        <div className="flex border-2 border-black m-4 rounded-lg h-48 w-48 items-center justify-center">
+          <h1>
+            {first}
+          </h1>
+        </div>
+        <div className="flex border-2 border-black m-4 rounded-lg h-48 w-48 items-center justify-center">
+          <h1>
+            {second}
+          </h1>
+        </div>
+      </div>
     </div>
   );
 };
