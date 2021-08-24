4521a165a32abb110ee3bc6588baa3de28720c75
bwozniak <bwozniak@umich.edu>
2021-08-23 21:28:45 -0400

    first commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Hello } from './Hello.jsx';
+import { Info } from './Info.jsx';
+
+export const App = () => (
+  <div>
+    <h1 className="text-red-500 text-2xl">Welcome to Meteor</h1>
+    <Hello/>
+    <Info/>
+  </div>
+);
